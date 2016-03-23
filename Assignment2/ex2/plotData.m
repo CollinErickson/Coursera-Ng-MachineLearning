7b252d876ce2da80f5b8026c6c45c017f9e59a81
Collin Erickson <collinberickson@gmail.com>
2016-03-23 13:35:10 -0500

    Completed assignment 2

--- Assignment2/ex2/plotData.m
+++ Assignment2/ex2/plotData.m
@@ -13,10 +13,15 @@ figure; hold on;
 %
 
 
+% get right indices
+pos = find(y==1);
+neg = find(y==0);
 
-
-
-
+% plot them
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, ...
+'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', ...
+'MarkerSize', 7);
 
 
 
